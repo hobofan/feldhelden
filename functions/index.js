ed5cc314bdbf2e3aaeab092483be38eed17c4269
Maximilian Goisser <goisser94@gmail.com>
2020-03-20 22:15:28 +0100

    Add basic connection to FaunaDB

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,28 @@
+import { GraphQLClient } from 'graphql-request';
+
 const handleApiRequest = async () => {
-  return {
-    message: 'hello world',
-  };
+  const endpoint = 'https://graphql.fauna.com/graphql'
+  const token = FAUNADB_SECRET; // Via `wranger secret`
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  })
+
+  const query = /* GraphQL */ `
+    {
+      viewer {
+        _id
+        firstName
+        lastName
+      }
+    }
+  `
+
+  const data = await graphQLClient.request(query)
+
+  return data;
 };
 
 export {handleApiRequest};
